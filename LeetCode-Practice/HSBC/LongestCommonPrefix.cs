50fe0dc8b11de1d25e6f9729302ed03d6c271f72
Cleef <cleefgulfan312@gmail.com>
2025-09-28 21:24:53 +0100

    Added LongestCommonPrefix.cs

---
+++ LeetCode-Practice/HSBC/LongestCommonPrefix.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode_Practice.HSBC;
+
+public class LongestCommonPrefix
+{
+    public string LCP(string[] strs) 
+    {
+        if(strs.Length == 0) return "";
+        if(strs.Length == 1) return strs[0];
+        var previousWord = strs[0];
+
+        for(int i = 0; i < previousWord.Length; i++)
+        {
+            for(int wordIndex = 1; wordIndex < strs.Length; wordIndex++)
+            {
+                if(previousWord.Length == 0) return "";
+
+                var word = strs[wordIndex];
+                if(i == word.Length || word[i] != previousWord[i])
+                {
+                    return previousWord[0..i];
+                }
+            }
+        }
+
+        return previousWord;
+    }
+}
