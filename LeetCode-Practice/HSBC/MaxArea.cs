3cf64b5685be1cd5b0bd541aab20f6db5ea184c6
Cleef <cleefgulfan312@gmail.com>
2025-09-30 23:21:04 +0100

    Added MaxArea

---
+++ LeetCode-Practice/HSBC/MaxArea.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode_Practice.HSBC;
+
+public class MaxArea
+{
+    public int MaxAreaSolution(int[] height)
+    {
+        var leftPointer = 0;
+        var rightPointer = height.Length - 1;
+        var maxArea = 0;
+
+        while (leftPointer < rightPointer)
+        {
+            // Math.Max(area, width * smallest height)
+            maxArea = Math.Max(maxArea, (rightPointer - leftPointer) * Math.Min(height[leftPointer], height[rightPointer]));
+
+            if (height[leftPointer] < height[rightPointer])
+            {
+                leftPointer++;
+            }
+            else
+            {
+                rightPointer--;
+            }
+        }
+        
+        return maxArea;
+    }
+}
