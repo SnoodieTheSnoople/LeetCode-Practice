e1190d163693c3ac8b56db44fbcb55169589b93b
Cleef <cleefgulfan312@gmail.com>
2025-10-02 15:18:05 +0100

    Added MissingCounter

---
+++ LeetCode-Practice/Codility Problems/MissingInteger.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode_Practice.Codility_Problems;
+
+public class MissingInteger
+{
+    public int MI(int[] A)
+    {
+        var set = A.ToHashSet();
+        var counter = 1;
+
+        while (set.Contains(counter))
+        {
+            counter++;
+        }
+        
+        return counter;
+    }
+}
