8c9a42e9e757771ac80f631acd2348f2bffda817
Cleef <cleefgulfan312@gmail.com>
2025-10-02 15:47:05 +0100

    Added FrogJump

---
+++ LeetCode-Practice/Codility Problems/FrogJump.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode_Practice.Codility_Problems;
+
+public class FrogJump
+{
+    public int FG(int x, int y, int d)
+    {
+        var distanceToCover = y - x;
+        var wholeJumps = distanceToCover / d;
+
+        var positionAfterWholeJumps = x + wholeJumps * d;
+
+        if (positionAfterWholeJumps < y) return positionAfterWholeJumps + 1;
+        
+        return positionAfterWholeJumps;
+    }
+}
