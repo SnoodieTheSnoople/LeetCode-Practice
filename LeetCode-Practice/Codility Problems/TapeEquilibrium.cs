74086e848acbbbc3648319455e8a9a65e5cf3e84
Cleef <cleefgulfan312@gmail.com>
2025-10-02 16:54:10 +0100

    Added TapeEquilibrium.cs

---
+++ LeetCode-Practice/Codility Problems/TapeEquilibrium.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode_Practice.Codility_Problems;
+
+public class TapeEquilibrium
+{
+    public int Solution(int[] A) 
+    {
+        var total = 0;
+
+        foreach(var num in A)
+        {
+            total+= num;
+        }
+
+        var leftSum = 0;
+        var minDiff = 2000;
+
+        for(int i = 0; i < A.Length - 1; i++)
+        {
+            leftSum += A[i];
+            var rightSum = total - leftSum;
+            var diff = Math.Abs(leftSum - rightSum);
+
+            if (minDiff > diff) minDiff = diff;
+        }
+
+        return minDiff;
+    }
+}
