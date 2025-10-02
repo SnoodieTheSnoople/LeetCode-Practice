99eeaf1bcd4543099fa372dea2670d5fdf421c20
Cleef <cleefgulfan312@gmail.com>
2025-10-02 23:01:23 +0100

    Added PermMissingElem.cs

---
+++ LeetCode-Practice/Codility Problems/PermMissingElem.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode_Practice.Codility_Problems;
+
+public class PermMissingElem
+{
+    public int solution(int[] A)
+    {
+        var N = A.Length + 1;
+        var xor = 0;
+
+        for (int i = 1; i <= N; i++)
+        {
+            xor ^= i;
+        }
+
+        foreach (var num in A)
+        {
+            xor ^= num;
+        }
+
+        return xor;
+    }
+}
