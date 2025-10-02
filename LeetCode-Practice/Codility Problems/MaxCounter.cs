0b1b0d6dc1d56ae03bb3ab6cc16e3c49131761a9
Cleef <cleefgulfan312@gmail.com>
2025-10-02 22:59:21 +0100

    Added MaxCounter

---
+++ LeetCode-Practice/Codility Problems/MaxCounter.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode_Practice.Codility_Problems;
+
+public class MaxCounter
+{
+    public int[] solution(int N, int[] A) {
+        // Implement your solution here
+        var counterArr = new int[N];
+        var baseCount = 0;
+        var maxCount = 0;
+
+        foreach (var val in A)
+        {
+            if (1 <= val && val <= N)
+            {
+                var index = val - 1;
+
+                if(counterArr[index] < baseCount)
+                {
+                    counterArr[index] = baseCount;
+                }
+
+                counterArr[index]++;
+
+                if (counterArr[index] > maxCount)
+                {
+                    maxCount = counterArr[index];
+                }
+            }
+            else if (val == N + 1)
+            {
+                baseCount = maxCount;
+            }
+        }
+
+        for (var i = 0; i < counterArr.Length; i++)
+        {
+            if (counterArr[i] < baseCount)
+            {
+                counterArr[i] = baseCount;
+            }
+        }
+
+        return counterArr;
+    }
+}
