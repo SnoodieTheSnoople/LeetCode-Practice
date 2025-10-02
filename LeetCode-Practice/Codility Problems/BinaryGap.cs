1531abf980148cf0742a225ec6506c318d82c1ce
Cleef <cleefgulfan312@gmail.com>
2025-10-02 14:41:03 +0100

    Implemented BinaryGap

--- LeetCode-Practice/Codility Problems/BinaryGap.cs
+++ LeetCode-Practice/Codility Problems/BinaryGap.cs
@@ -2,5 +2,27 @@
 
 public class BinaryGap
 {
-    
+    public int BG(int N)
+    {
+        var binaryList = new List<int>();
+        while (N > 0)
+        {
+            binaryList.Add(N % 2);
+            N /= 2;
+        }
+
+        var maxCounter = 0;
+        var counter = 0;
+
+        for (int i = binaryList.Count - 1; i > 0; i--)
+        {
+            if (binaryList[i] == 0) counter++;
+            else
+            {
+                maxCounter = Math.Max(maxCounter, counter);
+                counter = 0;
+            }
+        }
+        return maxCounter;
+    }
 }
