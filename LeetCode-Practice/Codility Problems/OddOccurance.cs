674c7172d28a7872011a920354e621367b88ca1d
Cleef <cleefgulfan312@gmail.com>
2025-10-02 22:06:24 +0100

    Added OddOccurance.cs

---
+++ LeetCode-Practice/Codility Problems/OddOccurance.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode_Practice.Codility_Problems;
+
+public class OddOccurance
+{
+    public int solution(int[] A) {
+        // Implement your solution here
+        if (A.Length == 1) return A[0];
+
+        System.Array.Sort(A);
+
+        for (int i = 0; i < A.Length - 1; i += 2)
+        {
+            if (A[i] != A[i+1])
+                return A[i];
+        }
+        return A[^1];
+    }
+
+    public int solutionB(int[] A)
+    {
+        var xorResult = 0;
+        foreach (var item in A)
+        {
+            xorResult ^= item;
+        }
+        return xorResult;
+    }
+}
