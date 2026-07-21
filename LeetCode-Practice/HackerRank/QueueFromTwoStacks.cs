e2db48bfae236089be3d4a425470e7258f40e399
cleef <cleefgulfan312@gmail.com>
2026-07-21 19:18:01 +0100

    Added QueueFromTwoStacks.cs

---
+++ LeetCode-Practice/HackerRank/QueueFromTwoStacks.cs
@@ -0,0 +1,48 @@
+namespace LeetCode_Practice;
+
+public class QueueFromTwoStacks
+{
+    public static List<int> processRequestQueueOperations(List<string> operations, List<int> values)
+    {
+        var inStack = new Stack<int>();
+        var outStack = new Stack<int>();
+        
+        var outputEvents = new List<int>();
+        
+        for (int i = 0; i < operations.Count; i++)
+        {
+            if (operations[i].Equals("enqueue"))
+            {
+                inStack.Push(values[i]);
+            }
+            else if (operations[i].Equals("dequeue"))
+            {
+                if (outStack.Count == 0)
+                {
+                    while(inStack.Count > 0)
+                    {
+                        outStack.Push(inStack.Pop());
+                    }
+                }
+                outputEvents.Add(outStack.Pop());
+                
+            }
+            else if (operations[i].Equals("peek"))
+            {
+                if (outStack.Count == 0)
+                {
+                    while(inStack.Count > 0)
+                    {
+                        outStack.Push(inStack.Pop());
+                    }
+                }
+                outputEvents.Add(outStack.Peek());
+            }
+            else if (operations[i].Equals("size"))
+            {
+                outputEvents.Add(inStack.Count + outStack.Count);
+            }
+        }
+        return outputEvents;
+    }
+}
